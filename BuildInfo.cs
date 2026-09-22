90dcc198d9f6971adb176b52ea2fd1f445310f91
camdendees57-maker <camdendees57@gmail.com>
2026-09-21 20:23:04 -0500

    Add WallWalker mod project: BuildInfo.cs

---
+++ BuildInfo.cs
@@ -0,0 +1,9 @@
+namespace WallWalker;
+
+internal static class BuildInfo
+{
+    internal const string Name = "WallWalker";
+    internal const string Description = "Touch or grab a surface to walk on walls and ceilings.";
+    internal const string Author = "Camden Dees";
+    internal const string Version = "1.0.0";
+}
